9dcb328f165156ec6fd6bb1d42057b4798013bb2
Darlan Marangoni <marangoni.darlan1@gmail.com>
2022-10-29 09:54:06 -0300

    core: java regex 1

--- src/main/java/core/java_regex/IPValidation.java
+++ src/main/java/core/java_regex/IPValidation.java
@@ -1,12 +1,11 @@
 package core.java_regex;
 
-import javax.swing.*;
 import java.util.Scanner;
 
-public class Solution {
-    public static void main(String[] args){
-        try(Scanner in = new Scanner(System.in)) {
-            while(in.hasNext()){
+public class IPValidation {
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            while (in.hasNext()) {
                 String IP = in.next();
                 System.out.println(IP.matches(new MyRegex().pattern));
             }
